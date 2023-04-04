ec158e05888f100788b318be54565d8c38e01ceb
GIL <gil.00000032610@unicap.br>
2023-04-04 16:48:32 -0300

    TESTING NON CONVERGING SYSTEM

--- src/Gauss/App.java
+++ src/Gauss/App.java
@@ -3,8 +3,9 @@ package Gauss;
 public class App {
     public static void main(String[] args) {
         GaussJacobiIterative gauss = new GaussJacobiIterative();
-        double[][] matrix = { { 6, -1, -1, +4 }, { 1, -10, 2, -1 }, { 3, -2, 8, -1 }, { 1, 1, 1, -5 } };
-        double[] result = { 17, -17, 19,-14 };
+        double[][] matrix = { { -2, 6, 6, -2, 5 }, { 3, -3, -8, -1, -8 }, { 6, -8, -8, 8, 2 }, { 4, -4, 6, 5, -1 },
+                { -6, -7, 8, -1, 9 } };
+        double[] result = { -48, 15, 36, 73, 37 };
         gauss.setMatrix(matrix);
         gauss.setResult(result);
         System.out.println(gauss);
