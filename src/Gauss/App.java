2158b40c1f7fd233afa22f438ce8c6210485a65d
GIL <gil.00000032610@unicap.br>
2023-04-04 16:18:23 -0300

    SISTEMAS COM MAIS DE 3 VARIAVÉIS

--- src/Gauss/App.java
+++ src/Gauss/App.java
@@ -5,10 +5,8 @@ public class App {
         GaussJacobiIterative gauss = new GaussJacobiIterative();
         double[][] matrix = { { 6, -1, -1, +4 }, { 1, -10, 2, -1 }, { 3, -2, 8, -1 }, { 1, 1, 1, -5 } };
         double[] result = { 17, -17, 19,-14 };
-
         gauss.setMatrix(matrix);
         gauss.setResult(result);
         System.out.println(gauss);
-
     }
 }
--- src/Gauss/GaussJacobiIterative.java
+++ src/Gauss/GaussJacobiIterative.java
@@ -49,7 +49,7 @@ public class GaussJacobiIterative {
 
     public void setResult(double[] result) {
         this.result = result;
-            findSolution(result);
+        findSolution(result);
     }
 
     public void setMatrix(double[][] matrix) {
@@ -59,26 +59,19 @@ public class GaussJacobiIterative {
     private void findSolution(double[] result) {
         double[] variables = new double[result.length];
 
-        for (int j = 0; !isSolution(result, variables); j++) {
+        for (int j = 0; !isSolution(result, variables)|| converge<20; j++) {
             int currentPos = j % variables.length;
             converge = j;
-            switch (currentPos) {
-                case 0 ->
-                    variables[currentPos] = (-variables[1] * matrix[0][1] - variables[2] * matrix[0][2]
-                            - variables[3] * matrix[0][3] + result[0])
-                            / matrix[0][0];
-                case 1 ->
-                    variables[currentPos] = (-variables[0] * matrix[1][0] - variables[2] * matrix[1][2]
-                            - variables[3] * matrix[1][3] + result[1])
-                            / matrix[1][1];
-                case 2 ->
-                    variables[currentPos] = (-variables[0] * matrix[2][0] - variables[1] * matrix[2][1]
-                            - variables[3] * matrix[2][3] + result[2])
-                            / matrix[2][2];
-                case 3 ->
-                    variables[currentPos] = (-variables[0] * matrix[3][0] - variables[1] * matrix[3][1]
-                            - variables[2] * matrix[3][2] + result[3]) / matrix[3][3];
-                default -> variables = null;
+            double operation = .0;
+            double coeficient = .0;
+            for (int i = 0; i < variables.length; i++) {
+                if (i != currentPos) {
+                    operation+= (-variables[i] * matrix[currentPos][i]) / matrix[currentPos][currentPos];
+                }
+                if (i == variables.length - 1) {
+                    coeficient+= result[currentPos] / matrix[currentPos][currentPos];
+                    variables[currentPos] = operation+coeficient;
+                }
             }
         }
         this.variables = variables;
